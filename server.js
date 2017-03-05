f21db7f40737e448b894ac6e7a65ad230f3613ba
jbenjaminy <young.benjamin@gmail.com>
2017-03-05 00:30:02 -0600

    adds notes about simplyrets api to server.js and creates listings.js file

--- server.js
+++ server.js
@@ -1,22 +1,54 @@
-//NOTE: ES6 'import' statements cannot be used in the server - just use ES5
-//syntax instead ('require'). Other ES6 features should all be available.
 const express = require('express');
+const bodyParser = require('body-parser');
+// const register = require('./server-side/functions/register');
+// const login = require('./server-side/functions/login');
 
+const app = express();
+const jsonParser = bodyParser.json();
 const HOST = process.env.HOST;
 const PORT = process.env.PORT || 8080;
 
-const app = express();
-app.use(express.static("build"));
-
-app.get("/hello", (req, res) => {
-	res.json({message: "Hello, world!"});
+app.use(express.static('build'));
+app.use(jsonParser);
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
 });
 
-function run_server() {
+app.post('/register', (req, res) => {
+    // const credentials = req.body;
+    // register(credentials).then((err, user) => {
+    //     if (err) {
+    //         console.error(err);
+    //         return res.sendStatus(500);
+    //     }
+        res.json({});
+    // });
+});
+
+app.get('/property', (req, res) => {
+    // const credentials = req.body;
+    // login(credentials).then((err, user) => {
+    //     if (err) {
+    //         console.error(err);
+    //         return res.sendStatus(500);
+    //     }
+        res.json({});
+    // });
+	//  To view all valid content-types for making an OPTIONS, 
+	//  make a request to the SimplyRETS api root
+		// curl -XOPTIONS -u simplyrets:simplyrets https://api.simplyrets.com/
+	// https://api.simplyrets.com/
+	// basic auth: for testing, use simplyrets:simplyrets
+});
+
+function runServer() {
 	app.listen(PORT, HOST, err => {
 		if (err) return console.error(err);
 		console.log(`Listening on port ${PORT}`);
 	});
 }
 
-if (require.main === module) run_server();
+if (require.main === module) runServer();
